98dedb2d76a3f82b3aae6ca75d4bcaddae47547a
niosto <nicolasospinat@gmail.com>
2025-07-20 19:52:04 -0500

    First commit

---
+++ Tutorial01/main.java
@@ -0,0 +1,47 @@
+import java.io.File; 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class main {
+
+    public static void createFile(String filename){
+        File file = new File(filename);
+        try{
+            if(file.createNewFile())
+                System.out.println(file.getName() + " created");
+            else
+                System.out.println(file.getName() + " already exists, overwriting it");
+
+        } catch(IOException e){
+            System.out.println("An error ocurred");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void writeFile(String name, String filename){
+        try{
+            FileWriter fileWriter = new FileWriter(filename);
+            fileWriter.write(name);
+            fileWriter.close();
+            System.out.println("Wrote " + name + " to " + filename);
+
+        }catch(IOException e){
+            System.out.println("An error ocurred");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        
+        String name = scan.nextLine();
+        scan.close();
+
+        String filename = "file.txt";
+        
+        createFile(filename); 
+        writeFile(name, filename);
+   }
+}
